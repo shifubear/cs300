c3273be41cc852c638d2006420a0b39436590bef
shifubear <shionfukuzawa@gmail.com>
2020-03-11 20:57:29 +0000

    Finished lab 5

--- lab5/button.py
+++ lab5/button.py
@@ -1,28 +1,42 @@
+# Shion Fukuzawa and Lauren Ebels
+# button.py
+#
+# Upon button input, rotates servo motor to random position. 
+#
+
 import pigpio
+import random
+import time
 
 # Constants
-MOTOR = 18
-BUTTON = False    #  
+MOTOR  = 18
+BUTTON = 12
+BUTTONWAIT = False    # When false, waiting for button. When true, random move 
+DEBOUNCE = 1000 #debounce time, in us
 pi = pigpio.pi()
-if not pi.connected:
-    exit(0)
-pi.set_servo_pulsewidth(MOTOR, 0)
+
+def move_to_angle(degrees):
+    pw = (degrees * 1000/180) + 500
+    pi.set_servo_pulsewidth(MOTOR, pw)
+
+def intCallback(g, level, tick):
+    if level == 0:
+        #button press
+        pass
+    else:
+        angle = random.randint(-90, 90)
+        move_to_angle(angle)
+
+
+pressTick = pi.get_current_tick() #initializing var
+pi.set_mode(BUTTON, pigpio.INPUT)
+pi.set_glitch_filter(BUTTON, DEBOUNCE)
+pi.set_pull_up_down(BUTTON, pigpio.PUD_UP) #this depends on how the switch is connected. In this case it is between GPIO and GND
+cb = pi.callback(BUTTON, pigpio.EITHER_EDGE, intCallback)
+
 try:
     while True:
-        print('setting angle = -90 degrees')
-        pi.set_servo_pulsewidth(MOTOR, 1000)
-        time.sleep(DELAY)
-        print('setting angle = 0 degrees')
-        pi.set_servo_pulsewidth(MOTOR, 1500)
-        time.sleep(DELAY)
-        print('setting angle = 90 degrees')
-        pi.set_servo_pulsewidth(MOTOR, 2000)
-        time.sleep(DELAY)
-    while duty_cycle >= 0 and duty_cycle <=100:
-        duty_cycle = int(input('Enter a PWM duty cycle (enter -1 to end): '))
-        if duty_cycle == -1:
-            break
-        pwm.ChangeDutyCycle(duty_cycle)
-
+        time.sleep(1)
+    
 except KeyboardInterrupt:
     pi.stop()
